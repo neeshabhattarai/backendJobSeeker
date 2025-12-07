6445ec8803fd7fdaf57c7c0a716682175a65ffc6
Neesha <nishabhattari778@gmail.com>
2025-12-07 12:23:00 +0545

    Added server path

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ app.use(cors());
 // console.log(__dirname)
 app.use('/images',express.static(path.join(__dirname,'public','images')));
 app.get('/',(req,res)=>{
-    const filePath=file.readFile('./data.json','utf-8',(err,data)=>{
-        res.send(data);
-    }
+    const filePath = path.join(__dirname, "data.json");
+  fs.readFile(filePath, "utf-8", (err, data) => {
+    if (err) return res.status(500).json({ error: "File read failed" });
+    res.json(JSON.parse(data));
+  }
 )
 })
 module.exports=app;
