b3b57e8b042a148ac69b008ba34957d00bd95ebb
Neesha <nishabhattari778@gmail.com>
2025-12-07 12:27:26 +0545

    Added server path

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-const express =require('express');
-const cors=require('cors');
-const file=require('fs');
-const app=express();
-const path=require('path');
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
 app.use(cors());
-// console.log(__dirname)
-app.use('/images',express.static(path.join(__dirname,'public','images')));
-app.get('/',(req,res)=>{
-    const filePath = path.join(__dirname, "data.json");
-  fs.readFile(filePath, "utf-8", (err, data) => {
-    if (err) return res.status(500).json({ error: "File read failed" });
-    res.json(JSON.parse(data));
-  }
-)
-})
-module.exports=app;
+
+// Static images
+app.use("/images", express.static(path.join(__dirname, "public", "images")));
+
+// Endpoint
+app.get("/", (req, res) => {
+  const dataPath = path.join(__dirname, "data.json");
+  const data = fs.readFileSync(dataPath, "utf-8");
+  res.send(JSON.parse(data));
+});
+
+module.exports = app;
